cd7cf494f9dc53c19d6f855020cd9d9fc56c650b
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2022-09-15 10:28:19 -0400

    first commit

---
+++ BoozeDotNet/Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using BoozeDotNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoozeDotNet.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Category(String CategoryName)
+        {
+            if (CategoryName == null)
+            {
+                // return View("Index");
+                return RedirectToAction("Index");
+            }
+            // pass input into view
+            ViewBag.CategoryName = CategoryName;
+
+            // use product model to mock a list of products for display
+            var products = new List<Product>();
+            // make products with names of beer
+            for (int i = 0; i < 10; i++)
+            {
+                products.Add(new Product { ProductId = i, Name = "product " + i });
+            }
+
+
+            return View(products);
+        }
+    }
+}
---
+++ BoozeDotNet/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace BoozeDotNet.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        //public string Description { get; set; }
+        //public decimal Price { get; set; }
+        //public string ImageUrl { get; set; }
+        //public string ImageThumbnailUrl { get; set; }
+        //public bool IsPreferredProduct { get; set; }
+        //public bool InStock { get; set; }
+        //public int CategoryId { get; set; }
+        //public Category Category { get; set; }
+    }
+}
